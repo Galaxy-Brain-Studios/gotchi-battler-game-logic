0b74aab3fb4cbafba0ad002ddd7c8b39946fc407
immaterial1 <immaterial111@gmail.com>
2026-01-03 12:26:27 +0000

    Refactor rerunBattle script

--- scripts/rerunBattle.js
+++ scripts/rerunBattle.js
@@ -1,15 +1,15 @@
 const fs = require('fs')
 const path = require('path')
 
-const { logToInGameTeams } = require('../utils/transforms')
 const { battle } = require('..')
 
 const main = async () => {
-    const logs = require('./data/yield_fields_1_2.json')
-    const teams = logToInGameTeams(logs)
-    const result = await battle(teams[0], teams[1], logs.meta.seed)
-    
-    fs.writeFileSync(path.join(__dirname, 'output', `${logs.meta.timestamp}-${Date.now()}.json`), JSON.stringify(result, null, '\t'))
+    console.time('Rerun battle')
+    const logs = require('./data/battleTimeout_k71899.json')
+    const result = await battle(logs.teamA, logs.teamB, logs.meta.seed, { debug: true })
+
+    fs.writeFileSync(path.join(__dirname, 'output', `${logs.meta.timestamp}-${Date.now()}.json`), JSON.stringify(result, null, 4))
+    console.timeEnd('Rerun battle')
 }
 
 // node scripts/rerunBattle.js
